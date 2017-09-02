86152eac2bea0f290be390bd4be1e25dffff590e
James McCann <jamesredux@gmail.com>
2017-09-02 18:28:44 +0700

    load and saving done

--- hangman.rb
+++ hangman.rb
@@ -7,12 +7,13 @@ require "./saveload.rb"
 
 class Hangman
 	def initialize
-		@saved = false
+		@save_names = []
+		@save_id = 1
 		run_game
 	end
 	
 	def run_game
-		puts "Welcome to Hangman, please select 'N' for a new game or 'L' to load a previously saved game."
+		puts "Welcome to Hangman, please select 'N' for a new game or 'L' to load a previously saved game:"
 		user_choice = gets.chomp.downcase
 		if 
 			user_choice == 'n'
@@ -20,33 +21,71 @@ class Hangman
 			player1.play_game
 		elsif 
 			user_choice == 'l'
-			save_choice = choose_save
-			load_game(save_choice)
+			list_saves   #perhaps create new class for loading and save management?
 		else
-		 puts "I did not understant that choice, please input 'N' or 'L'"
+		 puts "I did not understand that choice, please input 'N' or 'L'"
 		 run_game
 		end 
 	end
 
-	def load_game(save_choice)  
+	def list_saves
+		puts "List of Saved Games:"
+			Dir.glob("./saves/*").each do |file|
+				@save_names<<file 
+			puts "Save Number #{@save_id} #{file}"
+				@save_id += 1
+			end
+
+		game_choice = get_load_choice(@save_id)
+		load_game(@save_names, game_choice)
 		
-		puts "Welcome Back" 
-		loaded_game = YAML::load(File.open(save_choice))
-		puts "You have had #{loaded_game.guess_count} guesses"
-		puts loaded_game.progress.join
-		loaded_game.play_game
+	end
+
+	def get_load_choice(save_id)
+	puts "Please choose which save you would like to load (number), or type \"n\" for a new game:"
+		choice = gets.chomp
+		
+		if choice.downcase == "n"  #this is not working
+			player1 = Game.new
+			player1.play_game
+		elsif choice.to_i.between?(1, @save_id)
+			puts "Loading save number  #{choice} please wait:"
+			puts ""
+			return choice.to_i
+		else
+			puts "I did not understand that, please enter a number or 'n' for a new game:"
+			choice = get_load_choice(@save_id)
+		end				
+
+
+		
+	end
+
+
+	def load_game(save_names, game_choice) 
+
+		file_index = game_choice - 1
+		file_name = find_file(save_names, file_index)
+
+			puts "Welcome Back\n\n" 
+			
+			loaded_game = YAML::load(File.open(file_name))
+		
+			puts "You have had #{loaded_game.guess_count} guesses"
+			puts loaded_game.progress.join
+		
+			loaded_game.play_game
 
 	end
 
-	def choose_save
-		puts "the modified time is"
-		modtime = File.mtime("./saves/save1.yaml")
-		puts modtime.strftime('%d-%m-%y %H:%M')
-		#this is a method that goes to save log and returns the file name for the save that the user chooses.
-		return "./saves/28Aug17_17:23.yaml" #this is just a placeholder to test ...
+	def find_file(save_names, file_index)
+		save_names.each_with_index do |value, index|
+			if index == file_index
+				return value
+			end
+		end
+		
 	end
-	
-
 
 	
 end
--- saveload.rb
+++ saveload.rb
@@ -4,14 +4,15 @@ class Game
 
 	def save_game
 		save_number =  1
+		prog_string = @progress.join
 		puts YAML::dump(self)
 		saved_data = YAML::dump(self)
 
 		Dir.mkdir("saves") unless Dir.exists?("saves")
 
 		puts "saving game.."
-		filename = "saves/save#{save_number}_#{Time.now.strftime('%d%b')}.yaml"  # use this? Time.now.strftime('%d%b%y_%H:%M')
-
+		filename = "saves/<>#{prog_string}<>#{Time.now.strftime('%d-%m-%y_%H:%M')}.yaml"  # use this? Time.now.strftime('%d%b%y_%H:%M')
+		#_#{Time.now.strftime('%d%b')}
 		File.open(filename, "w") do |file|
 			file.puts YAML::dump(self)
 
@@ -28,3 +29,37 @@ class Game
 
 
 end	
+
+=begin
+
+below is something I found in someoneelses solution, there is stuff in there i should understand.
+
+def load_game()
+    if File.exists?("../save") && Dir.glob("../save/*").length > 0
+      saves = Dir.glob("../save/*")
+      puts "", "Please enter a number:", "0. cancel"
+      saves.each_with_index do |save, index|
+        puts "#{index + 1}. #{File.basename(save).split(".")[0]}"
+      end
+      loop do
+        user_input = gets.chomp
+        user_input_san = sanitize_load_input(user_input, saves)
+        if user_input_san == :no_good
+          puts "Please enter a number corresponding to a saved game, or 0 to cancel and go back."
+        elsif user_input_san == 0
+          return
+        else
+          yaml = File.read(saves[user_input_san - 1])
+          game = YAML::load(yaml)
+          puts "", "Game loaded!"
+          game.game_start()
+          return
+        end
+      end
+    else
+      puts "", "No save game files found!"
+
+=end
+  
+
+
